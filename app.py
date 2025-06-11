06fea5ee9b4379ef78194084b710e59560acd70f
Shakeel <shakeeluetp1041@gmail.com>
2025-06-11 14:24:33 +0200

    code writeen for Febanacci series

---
+++ app.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+from pydantic import BaseModel # For data-valiadation
+
+app = FastAPI() #Instance
+
+class FibonacciInput(BaseModel):
+    count: int
+
+def generate_fibonacci(n: int):
+    a, b = 0, 1
+    result = []
+    for _ in range(n):
+        result.append(a)
+        a, b = b, a + b
+    return result
+
+@app.post("/fibonacci")
+def get_fibonacci(data: FibonacciInput):
+    count = data.count
+    if count < 0:
+        return {"error": "Count must be non-negative"}
+    return {"fibonacci": generate_fibonacci(count)}
